8ec360843108b6f0889394c739239a189d02fe6d
FabianA3 <git@github.com:FabianA3>
2021-04-23 21:34:15 -0500

    nueva carga

--- index.py
+++ index.py
@@ -1,14 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, session
 from flask_mysqldb import MySQL
 import bcrypt
-  
+from werkzeug.utils import secure_filename
+import os
+
+UPLOAD_FOLDER = '/static/imgProductos'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
 app = Flask(__name__)
 
-#Conexion a la base de datos
+# Conexion a la base de datos
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'Fabian'
 app.config['MYSQL_PASSWORD'] = 'fabian1997'
 app.config['MYSQL_DB'] = 'bd_tecno-agro'
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 mysql = MySQL(app)
 
 # Settings
@@ -16,7 +24,8 @@ app.secret_key = 'clavesecreta'
 
 # Semilla de encriptamiento
 semilla = bcrypt.gensalt()
-  
+
+# Rutas
 @app.route("/") 
 def index(): 
     if 'nombres' in session:
@@ -24,6 +33,67 @@ def index():
     else:
         return render_template('index.html')
 
+@app.route('/inicio')
+def Inicio():
+    if 'nombres' in session:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM `productos`')
+        data = cur.fetchall()
+        return render_template("inicio.html", productos = data)
+    else:
+        return render_template('login.html')
+
+@app.route('/misDatos')
+def MisDatos():
+    if 'nombres' in session:
+        return render_template('misDatos.html')
+    else:
+        return render_template('index.html')
+    
+@app.route('/privacidad')
+def Privacidad():
+    if 'nombres' in session:
+        return render_template('privacidad.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/seguridad')
+def Seguridad():
+    if 'nombres' in session:
+        return render_template('seguridad.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/comunicaciones')
+def Comunicaciones():
+    if 'nombres' in session:
+        return render_template('comunicaciones.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/vender')
+def Vender():
+    if 'nombres' in session:
+        return render_template('vender.html')
+    else:
+        return render_template('index.html')
+
+@app.route('/imagenes')
+def Imagenes():
+    if 'nombres' in session:
+        cur = mysql.connection.cursor()
+        cur.execute('SELECT * FROM `productos`')
+        data = cur.fetchall()
+        return render_template('imagenes.html', productos = data)
+    else:
+        return render_template('index.html')
+
+@app.route('/salir')
+def Salir():
+    session.clear()
+    return redirect(url_for('index'))
+
+# Funciones Crud Usuario
 @app.route('/login', methods=['GET', 'POST'])
 def Login():
     if request.method == 'GET':
@@ -46,14 +116,18 @@ def Login():
             password_encriptado_encode = usuario[6].encode()
 
             if (bcrypt.checkpw(password_encode,password_encriptado_encode)): 
-                session['id'] = usuario[0] 
-                session['nombres'] = usuario[1]        
-                session['correo'] = usuario[5]
-                session['direccion'] = usuario[4]  
+                session['idUsuario'] = usuario[0]
+                session['nombres'] = usuario[1]
                 session['documento'] = usuario[2]  
-                session['telefono'] = usuario[3]  
+                session['telefono'] = usuario[3]   
+                session['direccion'] = usuario[4]     
+                session['correo'] = usuario[5]
 
-                return render_template('inicio.html')
+                cur = mysql.connection.cursor()
+                cur.execute('SELECT * FROM `productos`')
+                data = cur.fetchall()
+
+                return render_template('inicio.html', productos = data)
             else:
                 flash('La contraseña es incorrecta!', 'danger')
                 return render_template("login.html")
@@ -83,32 +157,6 @@ def agregarUsuario():
             flash('Cuenta creada exitosamente', 'success')
 
         return redirect(url_for('Inicio'))
-
-@app.route('/inicio')
-def Inicio():
-    if 'nombres' in session:
-        return render_template("inicio.html")
-    else:
-        return render_template('login.html')
-
-@app.route('/misDatos')
-def MisDatos():
-    if 'nombres' in session:
-        return render_template('misDatos.html')
-    else:
-        return render_template('index.html')
-    
-@app.route('/privacidad')
-def Privacidad():
-    if 'nombres' in session:
-        return render_template('privacidad.html')
-    else:
-        return render_template('index.html')
-
-@app.route('/salir')
-def Salir():
-    session.clear()
-    return redirect(url_for('index'))
 
 @app.route('/eliminarUsuario/<string:id>')
 def eliminarUsuario(id):
@@ -159,7 +207,39 @@ def modificarDatosC(id):
     mysql.connection.commit()
     flash('Datos de la Cuenta Actualizados Exitosamente', 'success')
     session['correo'] = correo
-    return redirect(url_for('MisDatos'))
+    return redirect(url_for('Seguridad'))
 
+# Funciones crud productos
+@app.route('/agregarProducto', methods= ['POST'])
+def agregarProducto():
+    if request.method == 'POST':
+        nombreProducto = request.form['nombreProducto']
+        precioProducto = request.form['precio']
+        ubicacionProducto = request.form['ubicacion']
+        categoriasProducto = request.form['categorias']
+        descripcionProducto = request.form['descripcion']
+        disponibilidadProducto = request.form['disponibilidad']
+        check = request.form['checkEnvio']
+        session['checkEnvio'] = check
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO `productos`(`nombreProducto`, `precioProducto`, `categoriasProducto`, `ubicacionProducto`, `descripcionProducto`, `disponibilidadProducto`, `usuarios_idusuario`) VALUES (%s, %s, %s, %s, %s, %s, %s)', (nombreProducto, precioProducto, categoriasProducto, ubicacionProducto, descripcionProducto, disponibilidadProducto, session['idUsuario']))
+        mysql.connection.commit()
+        Inicio()
+        flash('Producto registrado exitosamente', 'success')
+
+        """ files = request.files.getlist('imagenes')
+        for file in files:
+            try:
+                filename = secure_filename(file.filename)
+                img = file.save(os.getcwd() + "/static/imgProductos/" + str(session['idProducto']) + ".jpg")
+                print('si hay imagen')
+            except FileNotFoundError:
+                return "error" """
+
+        return redirect(url_for('Vender'), check)
+
+    
+
+# Inicio app
 if __name__ == "__main__": 
         app.run(debug=True, port= 8000)   
